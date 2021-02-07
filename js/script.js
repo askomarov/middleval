84f5996cc288e690e6260542f735de3d07e2ca6b
askomarov <askomarov13@mail.ru>
2021-02-07 13:20:04 +0300

    удалил bootstrap

--- js/script.js
+++ js/script.js
@@ -7,9 +7,6 @@ var app = new Vue({
     midleVal: 0
   },
   methods: {
-    calcme: function () {
-      // return numMidle = (Number(this.num1) + Number(this.num2)) / 2;
-    },
     average: function () {
     },
     addInput() {
@@ -17,28 +14,17 @@ var app = new Vue({
       return this.items.push(newItem);
     },
     removeInput() {
-      // let newItem = this.items.length - 1;
       return this.items.pop();
     },
     inputValueCalc() {
       let inputs = document.querySelectorAll(".calculate__input-num");
-      console.log(inputs.length);
       let sum = 0;
       for (let i = 0; i < inputs.length; i++) {
         let inputVal = Number(inputs[i].value);
-        console.log(inputVal);
         sum += inputVal;
       }
       return this.midleVal = sum / inputs.length;
     },
-    // deleteInput() {
-    //   let delBtn = document.querySelectorAll(".calculate__del-input");
-    //   delBtn.forEach(function (item) {
-    //     item.addEventListener("click", function () {
-    //       item.parentNode.parentNode.removeChild(item.parentNode);
-    //     });
-    //   });
-    // }
   },
   computed: {
 
